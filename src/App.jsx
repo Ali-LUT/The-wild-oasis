18e1f739525378b8696c15a5d11e5536bdc03698
Ali Ranjbar <ali.ranjbar.lian@gmail.com>
2024-11-16 21:21:42 +0200

    Added input component and some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
